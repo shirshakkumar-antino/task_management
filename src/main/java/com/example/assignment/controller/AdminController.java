cdf6a596c5a9ac399a0ea37f1104da0532250fa3
shirshak.kumar@antino.com <shirshakkumar-antino>
2026-01-18 12:36:20 +0530

    feat:Admin can delete the user && can update the role of the user

--- src/main/java/com/example/assignment/controller/AdminController.java
+++ src/main/java/com/example/assignment/controller/AdminController.java
@@ -3,12 +3,18 @@ package com.example.assignment.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.assignment.model.ApiResponse;
+import com.example.assignment.model.UserRequest;
 import com.example.assignment.model.UserResponse;
 import com.example.assignment.service.AdminService;
 
@@ -29,4 +35,33 @@ public class AdminController {
         );
     }
 
+    @PutMapping("/users/{id}/role")
+    public ResponseEntity<ApiResponse<UserResponse>> updateUserRole(
+            @PathVariable Long id,
+            @RequestBody UserRequest request) {
+
+        UserResponse response = adminService.updateRole(id, request);
+
+        return ResponseEntity.ok(
+                new ApiResponse<>(true, response, null)
+        );
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<ApiResponse<UserResponse>> deleteUser(
+            @PathVariable Long id) {
+
+        try {
+            UserResponse deletedUser = adminService.deleteUser(id);
+
+            return ResponseEntity.ok(
+                    new ApiResponse<>(true, deletedUser, null)
+            );
+
+        } catch (RuntimeException e) {
+
+            return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                    .body(new ApiResponse<>(false, null, e.getMessage()));
+        }
+    }
 }
--- src/main/java/com/example/assignment/service/AdminService.java
+++ src/main/java/com/example/assignment/service/AdminService.java
@@ -1,14 +1,12 @@
 package com.example.assignment.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException.Unauthorized;
-
 import com.example.assignment.mapper.UserMapper;
 import com.example.assignment.model.User;
+import com.example.assignment.model.UserRequest;
 import com.example.assignment.model.UserResponse;
 import com.example.assignment.repository.UserRepository;
 
@@ -32,6 +30,57 @@ public class AdminService {
                 .stream()
                 .map(userMapper::toResponse)
                 .toList();
+    }
+
+    public UserResponse updateRole(Long userId, UserRequest request) {
+
+        User currentUser = getCurrentUser();
+
+        if (!"ADMIN".equals(currentUser.getRole())) {
+            throw new RuntimeException("Only ADMIN can update user roles");
+        }
+
+        String newRole = request.getRole().toUpperCase();
+        if (!newRole.equals("USER") && !newRole.equals("ADMIN")) {
+            throw new IllegalArgumentException("Role must be USER or ADMIN");
+        }
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        user.setRole(newRole);
+        userRepository.save(user);
+
+        UserResponse response = new UserResponse();
+        response.setName(user.getName());
+        response.setEmail(user.getEmail());
+        response.setRole(user.getRole());
+
+        return response;
+    }
+
+    public UserResponse deleteUser(Long userId) {
+
+        User currentUser = getCurrentUser();
+        if (!"ADMIN".equals(currentUser.getRole())) {
+            throw new RuntimeException("Only ADMIN can delete users");
+        }
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (user.getId().equals(currentUser.getId())) {
+            throw new RuntimeException("Admin cannot delete self");
+        }
+
+        userRepository.delete(user);
+
+        UserResponse response = new UserResponse();
+        response.setName(user.getName());
+        response.setEmail(user.getEmail());
+        response.setRole(user.getRole());
+
+        return response;
     }
 
 
